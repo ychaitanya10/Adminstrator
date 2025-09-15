0c2f83a03fee1fbfe976735fa0097b8207998a08
TharunkumarReddybolaula <tbolaula@gitam.in>
2025-09-15 10:47:44 +0000

    commit

--- Adminstrator/Migrations/20250915094350_bvhf.Designer.cs
+++ Adminstrator/Migrations/20250915094350_bvhf.Designer.cs
@@ -4,6 +4,7 @@ using Adminstrator.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Adminstrator.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250915094350_bvhf")]
+    partial class bvhf
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Adminstrator/Migrations/20250915094350_bvhf.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Adminstrator.Migrations
+{
+    /// <inheritdoc />
+    public partial class bvhf : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "ValidTo",
+                table: "PromotionCodes",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "ValidFrom",
+                table: "PromotionCodes",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CourseTitle",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.CreateTable(
+                name: "Feedbacks",
+                columns: table => new
+                {
+                    FeedBackID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EventID = table.Column<int>(type: "int", nullable: false),
+                    SpeakerID = table.Column<int>(type: "int", nullable: false),
+                    feedback_remarks = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feedbacks", x => x.FeedBackID);
+                    table.ForeignKey(
+                        name: "FK_Feedbacks_Events_EventID",
+                        column: x => x.EventID,
+                        principalTable: "Events",
+                        principalColumn: "EventId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Feedbacks_Speakers_SpeakerID",
+                        column: x => x.SpeakerID,
+                        principalTable: "Speakers",
+                        principalColumn: "SpeakerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feedbacks_EventID",
+                table: "Feedbacks",
+                column: "EventID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feedbacks_SpeakerID",
+                table: "Feedbacks",
+                column: "SpeakerID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Feedbacks");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ValidTo",
+                table: "PromotionCodes",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ValidFrom",
+                table: "PromotionCodes",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CourseTitle",
+                table: "Events",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
